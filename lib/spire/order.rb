4a5c04cde43aeb063983c6cf5a84e3f197526d23
Denis <ddujota@internationalsafety.com>
2018-12-09 23:37:30 -0500

    added payments to create and update methods with commewnts

--- lib/spire/order.rb
+++ lib/spire/order.rb
@@ -105,6 +105,7 @@ module Spire
       total_ordered: 'totalOrdered',
       gross_profit: 'grossProfit',
       items: 'items',
+      payments: 'payments',
       created_by: 'createdBy',
       modified_by: 'modifiedBy',
       created: 'created',
@@ -138,11 +139,11 @@ module Spire
       # @option options [Hash] :address this is the billing address for the customer
       # @option options [Hash] :shipping_address this is the shipping address that the order will be sent to (defaults to the billing address if none provided)
       # @option options [Array] :items this is an array of hashes that will accept an inventory item that will have a hash example input from the web client: items: [ { "inventory": {"id": 123} } ]
+      # this array can also accept a desctiption and comment object that will create a comment on the order itself ex: items: [{"description":"MAKE COMMENT THRU API","comment":"MAKE COMMENT THRU API"}]
       # @option options [String] :discount this is the discount percentage for that order
       # @option options [String] :freight this is the shipping cost for that order
       # @option options [String] :customerPO this is the purchase order number for internal use
       # @option options [String] :type this is used to distinguish between it is an order ("O") or quote ("Q")
-      # this array can also accept a desctiption and comment object that will create a comment on the order itself ex: items: [{"description":"MAKE COMMENT THRU API","comment":"MAKE COMMENT THRU API"}]
 
       # @raise [Spire::Error] if the order could not be created.
       #
@@ -157,7 +158,8 @@ module Spire
           'discount' => options[:discount],
           'freight' => options[:freight],
           'customerPO' => options[:customer_po],
-          'type' => options[:type]
+          'type' => options[:type],
+          'payments' => options[:payments]
         )
       end
     end
@@ -197,6 +199,7 @@ module Spire
     # @option fields [String] :gross_profit
     # @option fields [Array] :items This will be an array of hashes, where if inventory is null and a comment option is provided, it will create a "Comment" on the order instead of a line item
     # @option fields [String] :background_color - the sync color that we use in spire
+    #options fields [Array] :payments - accepts simple payment method code id, which denotes payment type, used for marking payment deposited.
     def update_fields(fields)
       # instead of going through each attribute on self, iterate through each item in field and update from there
       self.attributes.each do |k, v|
@@ -227,6 +230,7 @@ module Spire
         customerPO: customer_po || "",
         type: type || "O",
         hold: hold || ACTIVE,
+        payments: payments || [],
         backgroundColor: background_color || 16777215
       }
 
