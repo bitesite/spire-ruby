64f58a31107564f90304ca5933f7d4004a825f1c
Chris Francis <chris.francis@bitesite.ca>
2022-02-04 10:34:10 -0500

    Add message to spire errors

--- lib/spire/error.rb
+++ lib/spire/error.rb
@@ -2,8 +2,9 @@ module Spire
   class Error < StandardError
     attr_reader :status_code
 
-    def initialize(message, status_code=nil)
+    def initialize(message, status_code = nil)
       @status_code = status_code
+      @message = message
       super(message)
     end
   end
